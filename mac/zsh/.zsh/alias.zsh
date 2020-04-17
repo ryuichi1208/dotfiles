40e861f615565767bc9bd60035cd71a30bc32390
ryuichi1208 <ryucrosskey@gmail.com>
2020-04-17 15:01:38 +0900

    add alias ansible-commands

--- mac/zsh/.zsh/alias.zsh
+++ mac/zsh/.zsh/alias.zsh
@@ -110,3 +110,25 @@ alias lzd='lazydocker'
 
 ### Python
 alias pipallupgrade="pip freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs pip install -U"
+
+### ansible
+alias ap='ansible-playbook'
+alias apc='ansible-playbook --check'
+alias apcd='ansible-playbook --diff --check'
+alias apd='ansible-playbook --diff'
+alias al='ansible-pull'
+alias avc='ansible-vault create'
+alias ave='ansible-vault edit'
+alias avr='ansible-vault rekey'
+alias avenc='ansible-vault encrypt'
+alias avdec='ansible-vault decrypt'
+alias avv='ansible-vault view'
+alias aga='ansible-galaxy'
+alias agad='ansible-galaxy delete'
+alias agai='ansible-galaxy install'
+alias agaim='ansible-galaxy import'
+alias againfo='ansible-galaxy info'
+alias agal='ansible-galaxy list'
+alias agalogin='ansible-galaxy login'
+alias agar='ansible-galaxy remove'
+alias agas='ansible-galaxy search'
