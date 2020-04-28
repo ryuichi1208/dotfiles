28e62806217e6f8de2ab22a55a5136a33c21ca0e
Ryuichi Watanabe <ryucrosskey@gmail.com>
2020-04-28 10:32:22 +0900

    Create Makefile.pl

---
+++ linux/Makefile.pl
@@ -0,0 +1,6 @@
+use 5.008;
+
+use ExtUtils::MakeMaker;
+WriteMakefile( NAME         => 'perlrc',
+               VERSION_FROM => 'lib/perlrc.pm', # finds $VERSION
+               AUTHOR       => 'Salvador Fandiño <sfandino@yahoo.com>');
