548aab1cea04ae0e29b359ad6553fb178c9841fd
Ryuichi Watanabe <ryucrosskey@gmail.com>
2021-08-29 13:34:54 +0900

    Update setup.sh

--- linux/setup/setup.sh
+++ linux/setup/setup.sh
@@ -27,5 +27,8 @@ tar -xvf fzf-0.27.2-linux_amd64.tar.gz && mv fzf /usr/local/bin/
 wget https://github.com/ggreer/the_silver_searcher/archive/refs/tags/2.2.0.tar.gz
 tar -xvf 2.2.0.tar.gz && ./the_silver_searcher-2.2.0/build.sh && mv the_silver_searcher-2.2.0/ag /usr/local/bin
 
+curl -fsSL https://rpm.nodesource.com/setup_16.x | bash -
+yum install -y node
+
 systemctl stop firewalld
 setenforce 0
