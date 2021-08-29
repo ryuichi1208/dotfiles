cac96e19d7278f8fdc401d03c969a40bf6301291
Ryuichi Watanabe <ryucrosskey@gmail.com>
2021-08-29 13:22:07 +0900

    Update setup.sh

--- linux/setup/setup.sh
+++ linux/setup/setup.sh
@@ -25,7 +25,7 @@ curl -so ~/.zshrc https://raw.githubusercontent.com/ryuichi1208/dotfiles/master/
 wget https://github.com/junegunn/fzf/releases/download/0.27.2/fzf-0.27.2-linux_amd64.tar.gz
 tar -xvf fzf-0.27.2-linux_amd64.tar.gz && mv fzf /usr/local/bin/
 wget https://github.com/ggreer/the_silver_searcher/archive/refs/tags/2.2.0.tar.gz
-tar -xvf 2.2.0.tar.gz && ./the_silver_searcher-2.2.0/build.sh && mv mv the_silver_searcher-2.2.0/ag /usr/local/bin
+tar -xvf 2.2.0.tar.gz && ./the_silver_searcher-2.2.0/build.sh && mv the_silver_searcher-2.2.0/ag /usr/local/bin
 
 systemctl stop firewalld
 setenforce 0
