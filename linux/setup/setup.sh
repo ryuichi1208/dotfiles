85a96f325e4bde23d1e835d9d6ebb5bec07b2dce
Ryuichi Watanabe <ryucrosskey@gmail.com>
2021-08-29 12:46:14 +0900

    Update setup.sh

--- linux/setup/setup.sh
+++ linux/setup/setup.sh
@@ -2,10 +2,12 @@
 
 yum update -y
 yum install -y \
-    gcc \
-    vim \
     curl \
+    gcc \
+    git \
+    vim \
     zsh
 
 chsh -s $(which zsh)
 curl -so ~/.vimrc https://raw.githubusercontent.com/ryuichi1208/dotfiles/master/linux/vimrc
+curl -so ~/.zshrc https://raw.githubusercontent.com/ryuichi1208/dotfiles/master/linux/zshrc
