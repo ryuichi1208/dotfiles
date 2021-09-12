9bb8f43a1df862246b131384c163777db7ebcdb7
Ryuichi Watanabe <ryucrosskey@gmail.com>
2021-09-12 11:05:13 +0900

    Update bootstrap_master.sh

--- linux/setup/bootstrap_master.sh
+++ linux/setup/bootstrap_master.sh
@@ -31,3 +31,4 @@ sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
 systemctl enable --now kubelet
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
