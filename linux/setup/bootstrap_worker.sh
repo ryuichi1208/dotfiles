79eab36c8eaa364d7d81f617a1ee62f7acfa78d0
Ryuichi Watanabe <ryucrosskey@gmail.com>
2021-09-12 11:25:45 +0900

    Create bootstrap_worker.sh

---
+++ linux/setup/bootstrap_worker.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+setenforce 0
+systemctl stop firewalld
+systemctl start containerd.service
+
+cat <<EOF > /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sysctl --system
+
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
+
+yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
+
+systemctl enable --now kubelet
