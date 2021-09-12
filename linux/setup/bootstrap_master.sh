a2deca2727364b436cd5b14d30c35c5b34b48f36
Ryuichi Watanabe <ryucrosskey@gmail.com>
2021-09-12 10:24:02 +0900

    Create bootstrap_master.sh

---
+++ linux/setup/bootstrap_master.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+cat <<EOF > /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sysctl --system
+
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
+
+yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
+
+kubeadm init \
+    --apiserver-advertise-address 172.16.64.48 \
+    --pod-network-cidr 10.244.0.0/16 \
+    --service-cidr 172.16.130.0/24
+
+systemctl enable --now kubelet
