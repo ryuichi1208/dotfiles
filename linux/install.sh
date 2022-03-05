43d8293dd3c08efbb0cf5467c063dbbcc97942a3
Ryuichi Watanabe <ryucrosskey@gmail.com>
2022-03-05 20:42:08 +0900

    Update install.sh

--- linux/install.sh
+++ linux/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # usage:
-# curl -s https://raw.github.com/ryuichi1208/dotfilese/master/linux/install.sh | bash
+# curl -s https://raw.githubusercontent.com/ryuichi1208/dotfiles/master/linux/install.sh | bash
 
 systemctl stop firewalld
 setenforce 0
