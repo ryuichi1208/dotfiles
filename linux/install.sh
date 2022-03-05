48b56169d0c0c044b0a32df06dae03cc39ce8e82
Ryuichi Watanabe <ryucrosskey@gmail.com>
2022-03-05 20:41:53 +0900

    Update install.sh

--- linux/install.sh
+++ linux/install.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
+# usage:
+# curl -s https://raw.github.com/ryuichi1208/dotfilese/master/linux/install.sh | bash
 
-set(MAC_CODESIGN_ID "" CACHE STRING "Mac code-signing identity")
-set(MAC_INJECT_GET_TASK_ALLOW ON CACHE BOOL "Inject get-task-allow on Mac")
-
-curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "/tmp/awscli-bundle.zip"
-unzip /tmp/awscli-bundle.zip -d /tmp
-/tmp/awscli-bundle/install -b $DOTFILES/bin/aws
-rm -rf /tmp/awscli-bundle*
+systemctl stop firewalld
+setenforce 0
