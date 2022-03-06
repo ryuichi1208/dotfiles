51177b922137d003c463a8bba31e5ba82a8dbcdd
Ryuichi Watanabe <ryucrosskey@gmail.com>
2022-03-06 14:45:04 +0900

    Update install.sh

--- linux/install.sh
+++ linux/install.sh
@@ -32,7 +32,14 @@ yum install -y \
   perl-core \
   perl-local-lib \
   ruby-devel \
-  lua-devel
+  lua-devel \
+  ctags \
+  libxml2-devel \
+  libxslt-devel \
+  gd-devel \
+  GeoIP-devel \
+  gperftools-devel \
+  libatomic_ops
 
 yum-config-manager --add-repo=https://copr.fedorainfracloud.org/coprs/carlwgeorge/ripgrep/repo/epel-7/carlwgeorge-ripgrep-epel-7.repo
 yum -y install ripgrep
