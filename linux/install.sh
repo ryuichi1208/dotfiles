65153de9f4050d732b2b1b9b7e15427daf3da013
Ryuichi Watanabe <ryucrosskey@gmail.com>
2022-03-06 23:40:01 +0900

    Update install.sh

--- linux/install.sh
+++ linux/install.sh
@@ -39,7 +39,10 @@ yum install -y \
   gd-devel \
   GeoIP-devel \
   gperftools-devel \
-  libatomic_ops
+  libatomic_ops \
+  tcpdump \
+  telnet \
+
 
 yum-config-manager --add-repo=https://copr.fedorainfracloud.org/coprs/carlwgeorge/ripgrep/repo/epel-7/carlwgeorge-ripgrep-epel-7.repo
 yum -y install ripgrep
