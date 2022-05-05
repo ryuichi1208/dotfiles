ed9f98a18130d158dfc31c9a2a3340b955d354b9
Ryuichi Watanabe <ryucrosskey@gmail.com>
2022-05-05 10:55:41 +0900

    Update k8s-master.sh

--- linux/oth/k8s-master.sh
+++ linux/oth/k8s-master.sh
@@ -2,4 +2,6 @@
 # curl -s https://raw.githubusercontent.com/ryuichi1208/dotfiles/master/linux/oth/k8s-master.sh | bash
 
 apt update
-apt install docker.io
+apt install -y docker.io
+systemctl start docker && systemctl enable docker
+apt install -y apt-transport-https curl
