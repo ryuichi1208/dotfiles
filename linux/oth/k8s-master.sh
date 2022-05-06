9067f465a1a561f0807e9cf8b0edbd8496120871
Ryuichi Watanabe <ryucrosskey@gmail.com>
2022-05-06 14:01:40 +0900

    Update k8s-master.sh

--- linux/oth/k8s-master.sh
+++ linux/oth/k8s-master.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # curl -s https://raw.githubusercontent.com/ryuichi1208/dotfiles/master/linux/oth/k8s-master.sh | bash
 
+readonly K8S_VERSION="1.23.6-00"
+readonly POD_CIDR="10.244.0.0/16"
+
 function init() {
   modprobe overlay
   modprobe br_netfilter
@@ -37,9 +40,11 @@ function install_k8s() {
   curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
   echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
   apt-get update
-  apt-get install -y kubelet=1.23.6-00 kubeadm=1.23.6-00 kubectl=1.23.6-00 && apt-mark hold kubelet kubeadm kubectl
+  apt-get install -y kubelet=${K8S_VERSION} kubeadm=${K8S_VERSION} kubectl=${K8S_VERSION} && apt-mark hold kubelet kubeadm kubectl
+}
 
-  kubeadm init --pod-network-cidr=10.244.0.0/16
+function setup_master() {
+  kubeadm init --pod-network-cidr=${POD_CIDR}
   mkdir -p $HOME/.kube
   cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
   chown $(id -u):$(id -g) $HOME/.kube/config
@@ -50,10 +55,13 @@ function install_k8s() {
 function main() {
   init
   install_dockerd
-  # install_containerd
   install_k8s
-  
-  curl -s https://raw.githubusercontent.com/ryuichi1208/dotfiles/master/linux/install.sh | bash
+
+  if [[ $(hostname) =~ "master" ]]; then
+    setup_master
+  else
+    setup_worker
+  fi
 }
 
 main
