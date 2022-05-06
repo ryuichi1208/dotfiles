822ce03910cdb1f25fb6d95b3f0964e90b42e895
Ryuichi Watanabe <ryucrosskey@gmail.com>
2022-05-06 10:41:09 +0900

    Update k8s-master.sh

--- linux/oth/k8s-master.sh
+++ linux/oth/k8s-master.sh
@@ -16,7 +16,7 @@ sudo apt-get install -y apt-transport-https ca-certificates curl
 sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
-sudo apt-get install -y kubelet=1.23.6 kubeadm=1.23.6 kubectl=1.23.6
+sudo apt-get install -y kubelet=1.23.6-00 kubeadm=1.23.6-00 kubectl=1.23.6-00
 sudo apt-mark hold kubelet kubeadm kubectl
 
 
