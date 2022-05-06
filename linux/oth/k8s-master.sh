28bf8ddf69a2b135c10533129db3559f0c3b225e
Ryuichi Watanabe <ryucrosskey@gmail.com>
2022-05-06 10:50:09 +0900

    Update k8s-master.sh

--- linux/oth/k8s-master.sh
+++ linux/oth/k8s-master.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # curl -s https://raw.githubusercontent.com/ryuichi1208/dotfiles/master/linux/oth/k8s-master.sh | bash
 
-cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
-br_netfilter
-EOF
+modprobe overlay
+modprobe br_netfilter
 
-cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+cat > /etc/sysctl.d/99-kubernetes-cri.conf <<EOF
+net.bridge.bridge-nf-call-iptables  = 1
+net.ipv4.ip_forward                 = 1
 net.bridge.bridge-nf-call-ip6tables = 1
-net.bridge.bridge-nf-call-iptables = 1
 EOF
 sudo sysctl --system
 
