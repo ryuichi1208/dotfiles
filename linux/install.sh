3bbf662ab6fd21c8ff0c06a2fbc56ea9bd502baf
Ryuichi Watanabe <ryucrosskey@gmail.com>
2023-05-05 11:28:07 +0900

    Update install.sh

--- linux/install.sh
+++ linux/install.sh
@@ -82,7 +82,8 @@ function setup_dotfiles() {
   if [[ ! -d ~/dotfiles ]]; then
     cd ~
     git clone https://github.com/ryuichi1208/dotfiles.git
-    rm -f ~/.zshrc && ln -s ~/dotfiles/linux/zshrc ~/.zshrc
+    rm -f ~/.zshrc ~/.vimrc ~/.tmux.conf ~/.vim/UltiSnips/ ~/.gdbinit
+    ln -s ~/dotfiles/linux/zshrc ~/.zshrc
     ln -s ~/dotfiles/linux/vimrc ~/.vimrc
     ln -s ~/dotfiles/linux/tmux.conf ~/.tmux.conf
     ln -s ~/dotfiles/linux/.vim/go.snippets ~/.vim/UltiSnips/
