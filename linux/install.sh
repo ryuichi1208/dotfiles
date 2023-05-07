95b0fd47ca7c497b4c89a26386097ca25cc4e1e1
Ryuichi Watanabe <ryucrosskey@gmail.com>
2023-05-07 12:28:25 +0900

    Update install.sh

--- linux/install.sh
+++ linux/install.sh
@@ -72,7 +72,7 @@ function setup_cmd_tools() {
     git clone https://github.com/rupa/z.git ~/z
   fi
   
-  if [[ ! -f  ~/.vim/autoload/plug.vim]]; then
+  if [[ ! -f  ~/.vim/autoload/plug.vim ]]; then
     curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
     vim -c "PlugInstall" -c "q" -c "q"
